f63896903bab5fcb309ab80a8a98d302cb0d3c15
che <che.blancoo58@gmail.com>
2021-03-09 18:15:11 +0100

    Add dist to the project

--- src/Contactus.js
+++
@@ -1,5 +0,0 @@
-export default function contactUs() {
-  const listItem5 = document.createElement('div');
-  listItem5.innerHTML = 'Contact us';
-  return listItem5;
-}
--- src/Tasty.js
+++
@@ -1,6 +0,0 @@
-const tabOne = () => {
-  const navHeaderLeft = document.createElement('div');
-  navHeaderLeft.innerHTML = 'Tasty';
-  return navHeaderLeft;
-};
-export default tabOne;
